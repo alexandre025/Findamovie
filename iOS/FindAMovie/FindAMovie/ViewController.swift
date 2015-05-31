0cef5553948aed21e707e5a796e658c55465edbb
paulo17 <paulboiseau@gmail.com>
2015-05-31 21:25:59 +0200

    adding main button

--- iOS/FindAMovie/FindAMovie/ViewController.swift
+++ iOS/FindAMovie/FindAMovie/ViewController.swift
@@ -10,12 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var label: UILabel!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Do any additional setup after loading the view, typically from a nib.
     }
     
     override func didReceiveMemoryWarning() {
