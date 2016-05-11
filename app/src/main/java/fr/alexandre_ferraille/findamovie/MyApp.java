47513d2eef348ff3004beae80641303b2249ab69
alexandre025 <alexandre.ferraille@gmail.com>
2016-05-11 16:02:11 +0200

    Setup basics

--- app/src/main/java/fr/alexandre_ferraille/findamovie/MyApp.java
+++ app/src/main/java/fr/alexandre_ferraille/findamovie/MyApp.java
@@ -1,9 +1,55 @@
 package fr.alexandre_ferraille.findamovie;
 
 import android.app.Application;
+import android.content.Context;
+import android.media.Image;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+
+import java.util.Locale;
+
+import fr.alexandre_ferraille.findamovie.network.LruBitmapCache;
 
 /**
  * Created by alexandre on 11/05/16.
  */
 public class MyApp extends Application {
+
+    private static MyApp instance;
+    private static Context context;
+
+    private RequestQueue requestQueue;
+    private ImageLoader imageLoader;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        MyApp.instance = this;
+
+        MyApp.context = getApplicationContext();
+
+        requestQueue = Volley.newRequestQueue(this);
+
+        LruBitmapCache cache = new LruBitmapCache(8 * 1024 * 1024);
+        imageLoader = new ImageLoader(requestQueue, cache);
+    }
+
+    public static MyApp getInstance() { return instance; }
+
+    public static Context getContext() { return context; }
+
+    public RequestQueue getRequestQueue() { return requestQueue; }
+
+    public ImageLoader getImageLoader() { return imageLoader; }
+
+    public static String getLanguage() {
+        String language = Locale.getDefault().getLanguage();
+        if (!language.equals("fr"))
+            language = "en";
+        return language;
+    }
+
 }
