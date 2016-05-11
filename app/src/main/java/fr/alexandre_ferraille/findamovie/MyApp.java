5916b97200bf244db5cb78991c536d5d4b4d0889
alexandre025 <alexandre.ferraille@gmail.com>
2016-05-11 15:27:06 +0200

    Init Find a movie 2.0

---
+++ app/src/main/java/fr/alexandre_ferraille/findamovie/MyApp.java
@@ -0,0 +1,9 @@
+package fr.alexandre_ferraille.findamovie;
+
+import android.app.Application;
+
+/**
+ * Created by alexandre on 11/05/16.
+ */
+public class MyApp extends Application {
+}
---
+++ app/src/main/java/fr/alexandre_ferraille/findamovie/ui/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package fr.alexandre_ferraille.findamovie.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import fr.alexandre_ferraille.findamovie.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
