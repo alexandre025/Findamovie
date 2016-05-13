6b1133f23a3472408c43233c76780b2372531e78
alexandre025 <alexandre.ferraille@gmail.com>
2016-05-13 10:04:09 +0200

    Install realm

---
+++ app/src/main/java/fr/alexandre_ferraille/findamovie/network/UrlBuilder.java
@@ -0,0 +1,15 @@
+package fr.alexandre_ferraille.findamovie.network;
+
+import fr.alexandre_ferraille.findamovie.MyApp;
+
+/**
+ * Created by alexandre on 11/05/16.
+ */
+public class UrlBuilder {
+
+    private static final String BASE_URL = "";
+
+    private static final String API_KEY = "";
+
+
+}
