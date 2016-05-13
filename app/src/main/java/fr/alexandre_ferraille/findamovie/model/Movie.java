d15dfafa1722f1b3acfb71631d9ffb717e3aa2f1
alexandre025 <alexandre.ferraille@gmail.com>
2016-05-13 10:31:29 +0200

    Update realm, setup movie class

---
+++ app/src/main/java/fr/alexandre_ferraille/findamovie/model/Movie.java
@@ -0,0 +1,81 @@
+package fr.alexandre_ferraille.findamovie.model;
+
+import java.util.Date;
+
+import io.realm.RealmObject;
+
+/**
+ * Created by alexandre on 13/05/16.
+ */
+public class Movie extends RealmObject {
+
+    private int id, voteCount;
+    private String title, overview, posterPath;
+    private Date releaseDate;
+    private float popularity, voteAverage;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getVoteCount() {
+        return voteCount;
+    }
+
+    public void setVoteCount(int voteCount) {
+        this.voteCount = voteCount;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public float getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(float popularity) {
+        this.popularity = popularity;
+    }
+
+    public float getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(float voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+}
